f6611fcdc3950326c9d638c965a5e3a1f00a8d2a
Eric Acquah <ericacquah1720@gmail.com>
2023-06-27 20:48:55 +0000

    Running different file to identify bug

--- format_strings.c
+++ format_strings.c
@@ -8,74 +8,46 @@
  *Return: Number of bytes written
  */
 
-int _printf(char *format, ...)
+int _printf(const char *format, ...)
 {
 	va_list holders;
 	char letter;
 	char *words;
-	int i;
-	int len = 0;/*Keep count of number of bytes written*/
+	int i, len = 0;/*Keep count of number of bytes written*/
 
 	va_start(holders, format); /*Initailize varaible list*/
-
 	for (i = 0; format[i] != '\0'; i++)/*loop over the format string*/
 	{
-		/*Check for character specifier values*/
-		if (format[i] == '%' && format[i + 1] == 'c')
+		if (format[i] != '%')/*If not a specifier print out*/
+			len += _print(format[i]);
+
+		if (format[i] == '%' && format[i + 1] == 'c' && format[i + 1] != '\0')
 		{
-			letter = (char)va_arg(holders, int);
+			letter = (char)va_arg(holders, int);/*extract then typecast*/
 			len += _print(letter);
 			format++;
-			format++;
 		}
-		/*Checking for percentage sign to avoid confusion*/
-		else if (format[i] == '%' && format[i + 1] == '%')
+		else if (format[i] == '%' && format[i + 1] == 's')
+		{
+			if (format[i + 1] != '\0')/*make sure value is no null*/
+			{
+				words = (char *)va_arg(holders, int *);
+				len += print_words(words);
+				format++;
+			}
+		}
+		if (format[i] == '%' && format[i + 1] == '%')/*If % is 2 print only 1*/
 		{
 			len += _print('%');
 			format++;
-			format++;
 		}
-		/*Check for string specifiers*/
-		else if (format[i] == '%' && format[i + 1] == 's')
+		if (format[i] == '%' && format[i + 1] != 'c' && format[i + 1] != 's'
+		    && format[i + 1] != '%')
 		{
-			words = (char *)va_arg(holders, int *);
-			len += print_words(words);
-			format++;
+			len += _print('%');/*If the % is just a character print it*/
 			format++;
 		}
-
-		len += _print(format[i]);/*if no specifier found just print out*/
 	}
-	va_end(holders);
+	va_end(holders);/*end extraction of arguments*/
 	return (len);
-}
-
-/**
- *_print - print to standard output
- *@letter: character to be printed
- *Return: number of bytes written to stdout
- */
-
-int _print(char letter)
-{
-	return (write(1, &letter, 1));
-}
-
-
-/**
- *print_words - print strings
- *@word: string pointer
- *Return: lenght of string
- */
-
-int print_words(char *word)
-{
-	int counter;
-
-	for (counter = 0; word[counter] != '\0'; counter++)
-	{
-		_print(word[counter]);
-	}
-
-	return (counter);
 }
