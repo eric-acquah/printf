20925b9b03125726b658afaf3c38caaac1fdc26e
Eric Acquah <ericacquah1720@gmail.com>
2023-06-28 21:43:24 +0000

    last update to my local repo

--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 
 #include <unistd.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 
 int _printf(const char *format, ...);
@@ -11,5 +12,8 @@ int _print(char letter);
 
 int print_words(char *word);
 
+int print_number(int num);
+
+void *resize(unsigned int size, unsigned int elem, void *old);
 
 #endif
