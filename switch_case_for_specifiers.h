9e4968c04be6076892bea04b3bb585a574bc5d63
root <portiafiyado@gmail.com>
2023-06-28 14:18:58 -0700

    Breaking code into many chunks to avoid lenghty statements

--- switch_case_for_specifiers.h
+++ switch_case_for_specifiers.h
@@ -1,30 +1,40 @@
 #ifndef SWITCH
 #define SWITCH
 
-switch (format[i + 1] == 'c' ? 1 : format[i + 1] == 's' ? 2 : 3)
-			{
-			case 1:/*for character sspecifier*/
-				letter = (char)va_arg(holders, int);
-				len += _print(letter);
-				format++;
-				break;
-			case 2:/*checked for string specifier*/
-				if (format[i + 1] != '\0')
-				{
-					words = (char *)va_arg(holders, char *);
-					if (words == NULL)
-						len += print_words("(null)");
-					len += print_words(words);
-					format++;
-				}
-				break;
-			case 3:/*for double % sign*/
-				len += _print('%');
-				format++;
-				break;
-			default:
-				len += _print('%');
-				format++;
-			}
+switch (format[i + 1] == 'c' ? 1 : format[i + 1] == 's' ? 2 :
+        format[i + 1] == 'd' || format[i + 1] == 'i' ? 3 : 4)
+                        {
+                        case 1:/*for character sspecifier*/
+                                letter = (char)va_arg(holders, int);
+                                len += _print(letter);
+                                format++;
+                                break;
+                        case 2:/*checked for string specifier*/
+                                if (format[i + 1] != '\0')
+                                {
+                                        words = (char *)va_arg(holders, char *);
+                                        if (words == NULL)
+                                                len += print_words("(null)");
+                                        len += print_words(words);
+                                        format++;
+                                }
+                                break;
+                        case 3:/*for decimal sspecifier*/
+                                decimal = va_arg(holders, int);
+                                len += print_number(decimal);
+                                format++;
+                                break;
+                        case 4:/*for double % sign*/
+                                len += _print('%');
+                                format++;
+                                break;
+			 case 4:/*for double % sign*/
+                                len += _print('%');
+                                format++;
+                                break;
+                        default:
+                                len += _print('%');
+                                format++;
+                        }
 
 #endif
