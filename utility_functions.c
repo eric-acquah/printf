34ef26dfbf99f426e4bcfd61726f8017675df2d3
root <portiafiyado@gmail.com>
2023-06-28 13:41:46 -0700

    adding a custome realloc function to resize dynamic memory space

---
+++ utility_functions.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ *resize - resize dynamically allocated memory space
+ *@size: new memory size
+ *@elem: number of elements in old memory block
+ *@old: pointer to old memory block
+ *Return: pointer to new block
+ */
+
+void *resize(unsigned int size, unsigned int elem, void *old)
+{
+        unsigned int i;
+        void *new;
+        char *clone;
+        char *clone1;
+
+
+        if (old == NULL)
+                return (NULL);
+
+        new = malloc(size);
+
+        if (new == NULL)
+                return (NULL);
+
+        clone = (char *)old;
+        clone1 = (char *)new;
+
+	 for (i = 0; i < size && i < elem; i++)
+        {
+                clone1[i] = clone[i];
+        }
+
+        free(old);
+
+        return (new);
+}
