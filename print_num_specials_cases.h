8878dca9092a843140a9ec49ce4027fb732f0d99
root <portiafiyado@gmail.com>
2023-06-28 14:09:07 -0700

    printing numbers with special cases

---
+++ print_num_specials_cases.h
@@ -0,0 +1,19 @@
+#ifndef SPECIAL
+#define SPECIAL
+
+if (num == 0)
+{
+        num_array[0] = '0'; /*Handel single zero digits*/
+        num_array[1] = '\0';
+}
+else
+{
+        if (num < 0)
+        {
+                num_array[0] = '-';/*Handel negative numbers*/
+                num = num * -1;
+                num_siz = 1;
+        }
+}
+
+#endif
